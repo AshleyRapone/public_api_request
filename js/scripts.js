dbf719e8b9541689ada24c37c43e103c8367528e
Ashley Rapone <ashleylynnrapone@gmail.com>
2021-06-11 20:03:08 -0400

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 // Gallery mockup
 
 function getEmployee() {
-    $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function(data) {
-        console.log(data);
-        const profile_image = data.results[0].picture.thumbnail;
-        const email = data.results[0].email;
-        const name = data.results[0].name;
-        const location = data.results[0].location;
-        formatUser(profile_image, email, name, location)
-        }
-    });
+  fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(
+         data => {
+            const info = data.results[0]
+            formatUser(info.picture.thumbnail, info.email, info.name, info.location)})
 }
+
+//function getEmployee() {
+//    $.ajax({
+//        url: 'https://randomuser.me/api/',
+//        dataType: 'json',
+//        success: function(data) {
+//        const profile_image = data.results[0].picture.thumbnail;
+//        const email = data.results[0].email;
+//        const name = data.results[0].name;
+//        const location = data.results[0].location;
+//        formatUser(profile_image, email, name, location)
+//        }
+//    });
+//}
 
 
 function country_or_state(location) {
@@ -35,7 +43,7 @@ function formatUser(profile_image, email, name, location) {
         </div>`
     gallery.insertAdjacentHTML('beforeend', html);
 }
-
+/*
 function displayEmployeeGallery(numberOfEmployees) {
   counter = 0
   while (counter < numberOfEmployees) {
@@ -44,5 +52,58 @@ function displayEmployeeGallery(numberOfEmployees) {
   }
 
 }
+*/
 
-displayEmployeeGallery(12);
+//const employeePromise = new Promise((resolve, reject) => {
+//    fetch('https://randomuser.me/api/')
+//    .then(response => response.json())
+//    .then(
+//         data => {
+//            const info = data.results[0]
+//            formatUser(info.picture.thumbnail, info.email, info.name, info.location)})  
+//    .then()  
+//})
+
+//employeePromise
+
+/*
+$('.gallery').on('click', '.card', function(event) {
+  const modalImage = event.currentTarget.querySelector('.card-img').src;
+  const modalName = event.currentTarget.querySelector('#name').textContent;
+  const modalEmail = event.currentTarget.querySelector('.card-text').textContent;
+  console.log(modalEmail);
+  const html = `
+    <div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${modalImage}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${modalName}</h3>
+            <p class="modal-text">${modalEmail}</p>
+            <p class="modal-text cap">city</p>
+            <hr>
+            <p class="modal-text">(555) 555-5555</p>
+            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+            <p class="modal-text">Birthday: 10/21/2015</p>
+        </div>
+    </div>  
+`
+   const newGallery = document.getElementById('gallery');
+   newGallery.insertAdjacentHTML('beforeend', html);
+});
+
+*/
+
+//displayEmployeeGallery(12);
+
+fetch('https://randomuser.me/api/?results=12')
+  .then(response => response.json())
+  .then(data => {displayEmployeeGallery(data.results)})
+
+
+function displayEmployeeGallery(employeeInfo) {
+  for (let i = 0; i < employeeInfo.length; i++) {
+    formatUser(employeeInfo[i].picture.thumbnail, employeeInfo[i].email, employeeInfo[i].name, employeeInfo[i].location)
+  }
+}
+
